b8d0102dae339a350721c0897f4f0b06dd14eff0
Tom Dicks <tom@cubeupload.com>
2022-08-19 15:20:46 +0100

    Added vscode debug config

--- shared/network.py
+++ shared/network.py
@@ -6,8 +6,18 @@ from twisted.protocols.amp import (
     Command, Integer, Float, String, Argument
 )
 
+
+"""
+Sent by the client to the server when connected.
+(clent to server)
+"""
 class Introduce(Command):
     response = [(b'identifier', Integer()),
                 (b'granularity', Integer()),
                 (b'x', Integer()),
                 (b'y', Integer())]
+
+"""
+class SetMyPosition(Command):
+    arguments = [(b'')]
+"""
