962820b52c56dba0545c6a92e6a12c6dd1318c4d
JudgementTaG <111437747+JudgementTaG@users.noreply.github.com>
2022-08-19 10:38:11 +0100

    Add files via upload

---
+++ client/level.py
@@ -0,0 +1,61 @@
+import random
+import pygame
+from Settings import *
+from Tiles import Tile
+from player import Player
+import pygame.sprite
+
+
+class Generator:
+    def __init__(self):
+        self.blank = ' '
+
+    def map_generation(seed_number=random.randint(0,10000)): # uses a random integer for the seed number unless the user supplies their own
+        map_base = [['X' for i in range(0, 20)] for j in range(0, 20)] # saturates a 20 x 20 grid with 'X'
+        random.seed(seed_number)
+        for i in range(1, 19):           #
+            for j in range(1, 19):       #   these 3 lines take the rows and columns 1 tile in from each side a saturates them as '_' (free space)
+                map_base[i][j] = '_'     #
+                if i in range(2,18) and j in range(2,18):
+                    if 'X' in [map_base[i - 1][j], map_base[i + 1][j], map_base[i][j - 1], map_base[i][j + 1]]:
+                        if random.randint(1,1000) % 6 == 0: # increses the chance of like-objects spawning if there is another present in any adjacent tile
+                            map_base[i][j] = 'X'
+                # lines 25-28 place objects in the free spaces if a random number is perfectly divisable
+                if random.randint(1, 1000) % 17 == 0:
+                    map_base[i][j] = 'X'
+                if random.randint(1, 1000) % 73 == 0:
+                    map_base[i][j] = 'T'
+            # rows 30 - 36 checks the map for empty tiles and appends them to a list as tuples then selects the player start location
+        open_ground = []
+        for r_index, row in enumerate(map_base):
+            for col_index, text in enumerate(row):
+                if text == '_':
+                    open_ground.append((r_index,col_index))
+        map_x,map_y = random.choice(open_ground)
+        map_base[map_x][map_y] = 'P'
+        for row in map_base: # prints map to console for debugging
+            print(row)
+        return map_base
+
+class Level:
+    def __init__(self):
+        self.display_surface = pygame.display.get_surface()
+        self.visible_sprites = pygame.sprite.Group()
+        self.obst_sprites = pygame.sprite.Group()
+        self.map = Generator.map_generation()
+        self.create_map()
+
+    def run(self):
+        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.update()
+
+    def create_map(self):
+        for row_indx, row in enumerate(self.map):
+            for col_indx, col in enumerate(row):
+                x = col_indx * TILESIZE # TILESIZE is stored in Settings.py
+                y = row_indx * TILESIZE
+                if col == 'X':
+                    Tile((x,y), [self.visible_sprites, self.obst_sprites]) # Tile is a member of both 'visible...' and 'obst...'
+                if col == 'P':
+                    print(f'x: {x}    y: {y}')
+                    Player((x,y), [self.visible_sprites], self.obst_sprites) # Player is a member of 'visible...' and only references 'obst...'
